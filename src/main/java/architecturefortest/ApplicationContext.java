3d4ad44bf332c2799bc846af4c2488075178f20e
Bruno Augusto <bruno.delgado1988@outlook.com>
2017-02-08 16:40:32 -0300

    Code refactoring to eliminate the need of declaring the resources at context initialization

--- src/main/java/architecturefortest/ApplicationContext.java
+++ src/main/java/architecturefortest/ApplicationContext.java
@@ -1,27 +1,41 @@
 package architecturefortest;
 
-import javax.inject.Named;
+import java.util.Set;
+
+import javax.ws.rs.Path;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.stereotype.Component;
 
 import architecturefortest.application.ShopperApplicationContext;
 import architecturefortest.application.ShopperDto;
-import architecturefortest.resource.CartResource;
 
 @SpringBootApplication
 @Configuration
 public class ApplicationContext {
 
-    @Named
+	@Component
     public static class ConfigRestResources extends ResourceConfig {
         public ConfigRestResources() {
-            this.register(CartResource.class);
+            ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(true);
+            scanner.addIncludeFilter(new AnnotationTypeFilter(Path.class));
+            Set<BeanDefinition> beans = scanner.findCandidateComponents("architecturefortest");
+            for (BeanDefinition beanDefinition : beans) {
+                try {
+                    this.registerClasses(Class.forName(beanDefinition.getBeanClassName()));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
